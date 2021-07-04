71bc3cfdd01af68dd8f238dee481cdab3ea07240
Akhilez <akhild18@yahoo.com>
2021-07-04 11:34:27 -0500

    Policy grad successful

--- rl_talk/train_pg_gw_2.py
+++ rl_talk/train_pg_gw_2.py
@@ -1,7 +1,22 @@
 import torch
+from omegaconf import DictConfig
 from torch import nn
 from torch.optim import Adam
 from envs.train_gridworld import GridWorldEnvWrapper
+import wandb
+from datetime import datetime
+from settings import BASE_DIR
+
+wandb.init(
+    name=f"gw_pg_{str(datetime.now().timestamp())[5:10]}",
+    project="rl_talk_gridworld",
+    config={},
+    save_code=True,
+    group=None,
+    tags=['policy_grad'],  # List of string tags
+    notes=None,  # longer description of run
+    dir=BASE_DIR,
+)
 
 env = GridWorldEnvWrapper()
 
@@ -12,7 +27,7 @@ model = nn.Sequential(
 )
 optimizer = Adam(model.parameters())
 
-for epoch in range(10):
+for episode in range(10000):
     state = env.reset()  # state shape: (4, 4, 4)
 
     rewards = []
@@ -29,21 +44,21 @@ for epoch in range(10):
         rewards.append(reward)
         probabilities.append(prob[0][action])
 
-        print('.', end='')
         if is_done:
             break
 
-    rewards = torch.tensor(rewards)
+    rewards = torch.FloatTensor(rewards)
     probabilities = torch.stack(probabilities)
     loss_terms = -1 * rewards * probabilities
 
-    # print(f'\n{rewards=}\n{probabilities=}\n{loss_terms=}')
-
     loss = torch.mean(loss_terms)
-    print(loss.item())
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-
+    log = DictConfig({"episode": episode})
+    log.ep_loss = loss.item()
+    log.ep_mean_reward = rewards.mean().item()
+    log.ep_length = len(rewards)
+    wandb.log(log)
